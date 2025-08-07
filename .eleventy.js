f38c329aa7df0a385f80c528c6b3fbcd9c03d5e8
Daniel Hwang <dawh@ines.home>
2025-08-07 03:39:41 -0500

    Add filters for post dates

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
+const { DateTime } = require("luxon");
+
 module.exports = (config) => {
   config.addPassthroughCopy("./src/styles");
   config.addPassthroughCopy({"./node_modules/@picocss/pico/css/pico.min.css": "styles/pico.min.css"});
   config.addWatchTarget("./src/styles/");
   
+  config.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  config.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
   // Returns a collection of blog posts in reverse order
   config.addCollection('blog', (collection) => {
     return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
