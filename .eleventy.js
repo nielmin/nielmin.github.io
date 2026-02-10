92a743ac3bc03fbc9ee4c3f4d6a1fbc117373e01
Daniel Hwang <dawh@ines.home>
2026-02-10 17:36:07 -0600

    Initial rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,25 @@
 const { DateTime } = require("luxon");
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 
 module.exports = (config) => {
+  config.addPlugin(feedPlugin, {
+    type: "atom", // or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "blog", // iterate over `collections.posts`
+			limit: 10,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Blog Title",
+			subtitle: "This is a longer description about your blog.",
+			base: "https://example.com/",
+			author: {
+				name: "Your Name",
+				email: "", // Optional
+			}
+		}
+  });
   config.addPassthroughCopy("static");
 
   config.addPassthroughCopy("static/img");
